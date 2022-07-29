3d2ca31395689a8e8cedb4edc69409b6d948da9d
javier <javier.apaez@gnastrategy.com>
2022-07-29 11:52:28 -0500

    Initial Commit

---
+++ TestiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestiOS
+//
+//  Created by javier apaez on 29/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
