5fa5314d4c7d12cb4606ca24f9e908f1816b687e
javier <javier.apaez@gnastrategy.com>
2022-07-29 21:44:24 -0500

    network manager

---
+++ TestiOS/Network/NetworkManager.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.swift
+//  TestiOS
+//
+//  Created by javier apaez on 29/07/22.
+//
+
+import Foundation
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    private var baseURL = "https://s3.amazonaws.com/dev.structure.files/examen/ios/test.json"
+    
+}
